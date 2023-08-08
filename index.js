a7ef962d16da51848507eeec931d230fbd4ec706
Sadiq <sadiq.said@student.moringaschool.com>
2023-08-08 12:28:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
-
+    const seenNumbers = new Set(); // To keep track of numbers we've seen
+    
+    for (let num of array) {
+      const complement = target - num; // Calculate the complement needed for the target sum
+      
+      if (seenNumbers.has(complement)) {
+        return true; // Found a pair that adds up to the target
+      }
+      
+      seenNumbers.add(num); // Add the current number to the set
+    }
+    
+    return false; // No pair found
+  }
+  
+  // Test cases
+  console.log(hasTargetSum([3, 19, 12, 0, 8, 7], 1)); // Output: false
+  console.log(hasTargetSum([5, 19, 4, 6, 30], 11));   // Output: true
+  console.log(hasTargetSum([2, 3, 7], 5));              // Output: true
+  
 /* 
   Write the Big O time complexity of your function here
+  The total time complexity of the above function is O(n), where n is the number of elements in the array.
 */
 
 /* 
